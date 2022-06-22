21b4068bcc8dbccbee9e23d393a1a17a6a604832
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-06-21 22:59:36 -0500

    NavigationBar style updated

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,21 +1,30 @@
 // import "bootstrap/dist/js/bootstrap.min.js";
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function NavigationBar() {
+  const [ underline, setUnderline ] = useState(false)
+
+  const location = useLocation()
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+  console.log(splitLocation)
+
   return (
     <div className="nav-wrapper">
       <ul className="main-menu">
-        <li><Link to="/">HOME</Link></li>
-        <li><Link to="/">SERVICES</Link></li>
-        <li><Link to="/projects">PROJECTS</Link></li>
-        <li><Link to="/">TESTIMONIALS</Link></li>
-        <li><Link to="/">BLOG</Link></li>
+        <li className={splitLocation[1] === "" ? "link-active": "" }><Link to="/">HOME</Link></li>
+        <li className={splitLocation[1] === "services" ? "link-active": "" }><Link to="/">SERVICES</Link></li>
+        <li className={splitLocation[1] === "projects" ? "link-active": "" }><Link to="/projects">PROJECTS</Link></li>
+        <li className={splitLocation[1] === "testimonials" ? "link-active": "" }><Link to="/">TESTIMONIALS</Link></li>
+        <li className={splitLocation[1] === "blog" ? "link-active": "" }><Link to="/">BLOG</Link></li>
       </ul>
 
       <ul className="contact-menu">
-        <li><Link to="/">ABOUT</Link></li>
-        <li><Link to="/">SHOP</Link></li>
-        <li><Link to="/">CONTACT</Link></li>
+        <li className={splitLocation[1] === "about" ? "link-active": "" }><Link to="/">ABOUT</Link></li>
+        <li className={splitLocation[1] === "shop" ? "link-active": "" }><Link to="/">SHOP</Link></li>
+        <li className={splitLocation[1] === "contact" ? "link-active": "" }><Link to="/">CONTACT</Link></li>
       </ul>
     </div>
   )
